44d79d26665557fe8afa25474075eaafc03e2a9a
Stilyan <stilian.rusev@gmail.com>
2019-09-03 10:17:42 +0200

    fresh start

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const cowFloat = TweenMax.to('.cow', 3, {y: 400, rotation: 5});
-const moopsFloat = TweenMax.to('.moops', 3, {opacity: 0.2, rotation:-2});
-const ufoFloat = TweenMax.to('.ufo', 5, {x: -20, rotation: 3});
-const cloudsFloat = TweenMax.to('.clouds', 90, {x: 60});
-const actionAppear = TweenMax.to('.action', 1, {opacity: 1});
+// const cowFloat = TweenMax.to('.cow', 3, {y: 400, rotation: 5});
+// const moopsFloat = TweenMax.to('.moops', 3, {opacity: 0.2, rotation:-2});
+// const ufoFloat = TweenMax.to('.ufo', 5, {x: -20, rotation: 3});
+// const cloudsFloat = TweenMax.to('.clouds', 90, {x: 60});
+// const actionAppear = TweenMax.to('.action', 1, {opacity: 1});
 
-cowFloat.yoyo(true).repeat(-1);
-moopsFloat.yoyo(true).repeat(-1);
-ufoFloat.yoyo(true).repeat(-1);
-cloudsFloat.yoyo(true).repeat(-1);
+// cowFloat.yoyo(true).repeat(-1);
+// moopsFloat.yoyo(true).repeat(-1);
+// ufoFloat.yoyo(true).repeat(-1);
+// cloudsFloat.yoyo(true).repeat(-1);
