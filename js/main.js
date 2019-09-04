ae4571fa19220f3b8c0168a3737b78eb57e03161
Zheni Denkova <zhenidenkova@Zhenis-MacBook-Pro.local>
2019-09-04 22:06:21 +0200

    home setup

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-const cowFloat = TweenMax.to('.cow', 3, {y: 10, rotation: 5});
-const moopsFloat = TweenMax.to('.moops', 4, {y: 5,opacity: 0.2, rotation:-2});
-const ufoFloat = TweenMax.to('.ufo', 5, {x: 5, rotation: 3});
-const cloudsFloat = TweenMax.to('.clouds', 90, {x: 60});
+// const cowFloat = TweenMax.to('.cow', 3, {y: 10, rotation: 5});
+// const moopsFloat = TweenMax.to('.moops', 4, {y: 5,opacity: 0.2, rotation:-2});
+// const ufoFloat = TweenMax.to('.ufo', 5, {x: 5, rotation: 3});
+// const cloudsFloat = TweenMax.to('.clouds', 90, {x: 60});
 
-cowFloat.yoyo(true).repeat(-1);
-moopsFloat.yoyo(true).repeat(-1);
-ufoFloat.yoyo(true).repeat(-1);
-cloudsFloat.yoyo(true).repeat(-1);
+// cowFloat.yoyo(true).repeat(-1);
+// moopsFloat.yoyo(true).repeat(-1);
+// ufoFloat.yoyo(true).repeat(-1);
+// cloudsFloat.yoyo(true).repeat(-1);
