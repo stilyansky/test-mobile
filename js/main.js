ea6f6efc91989f74ea79d2995ee675890b574b02
Stilyan <stilian.rusev@gmail.com>
2019-09-04 10:46:32 +0200

    mobile ready

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-// const cowFloat = TweenMax.to('.cow', 3, {y: 400, rotation: 5});
-// const moopsFloat = TweenMax.to('.moops', 3, {opacity: 0.2, rotation:-2});
-// const ufoFloat = TweenMax.to('.ufo', 5, {x: -20, rotation: 3});
-// const cloudsFloat = TweenMax.to('.clouds', 90, {x: 60});
-// const actionAppear = TweenMax.to('.action', 1, {opacity: 1});
+const cowFloat = TweenMax.to('.cow', 3, {y: 10, rotation: 5});
+const moopsFloat = TweenMax.to('.moops', 4, {y: 5,opacity: 0.2, rotation:-2});
+const ufoFloat = TweenMax.to('.ufo', 5, {x: 5, rotation: 3});
+const cloudsFloat = TweenMax.to('.clouds', 90, {x: 60});
 
-// cowFloat.yoyo(true).repeat(-1);
-// moopsFloat.yoyo(true).repeat(-1);
-// ufoFloat.yoyo(true).repeat(-1);
-// cloudsFloat.yoyo(true).repeat(-1);
+cowFloat.yoyo(true).repeat(-1);
+moopsFloat.yoyo(true).repeat(-1);
+ufoFloat.yoyo(true).repeat(-1);
+cloudsFloat.yoyo(true).repeat(-1);
